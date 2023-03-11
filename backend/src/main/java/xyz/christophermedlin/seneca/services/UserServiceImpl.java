2490114a5d0ee54c084b4279af00189d46237846
Christopher Medlin <christopherjmedlin@mailbox.org>
2023-03-11 16:08:10 -0700

    Add UserServiceImpl

---
+++ backend/src/main/java/xyz/christophermedlin/seneca/services/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package xyz.christophermedlin.seneca.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import xyz.christophermedlin.seneca.entities.User;
+import xyz.christophermedlin.seneca.repositories.UserRepository;
+
+public class UserServiceImpl implements UserService {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Override
+  public void createUser(String username, String password) {
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    User u = new User(username, encoder.encode(password));
+    userRepository.save(u);
+  }
+}
